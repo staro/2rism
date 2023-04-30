e75e91d28db143396b70454935032ecec6d28d08
Oleg Starichenko <staro.oleg@gmail.com>
2023-04-30 16:16:52 +0300

    add new sections

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-customSelect('select');
+customSelect("select");
 
 const picker = new easepick.create({
   // lang: 'ru-Ru',
-  element: document.getElementById('calendar'),
-  css: ['https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css'],
-  format: 'DD.MM.YY',
+  element: document.getElementById("calendar"),
+  css: ["https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"],
+  format: "DD.MM.YY",
 });
